83fe2f5ea3baa706c6aabb8ecf2cd0debdbd6751
Steven Baltakatei Sandoval <baltakatei@gmail.com>
2019-08-15 21:12:09 +0000

    Renamed pypop-p.py and pypop-v.py to prove.py and verify.py and settled on time.time_ns() for time measurement

--- local_benchmark.py
+++ local_benchmark.py
@@ -3,23 +3,80 @@
 
 import time
 
-lightspeed = 299792458
+# Set constants
+lightspeed  = 299792458    # speed of light in meters per second
+loop_count = 1000000
 
-#time.time()
-print("Testing time.time(). Spatial resolution (meters):")
-for counter in range(1,10):
-    time_1     = time.time()
-    time_2     = time.time()
-    distance   = lightspeed*(abs(time_1-time_2))
-    print(distance)
+print("")
 
-#time.time_ns()
-print("Testing time.time_ns(). Spatial resolution (meters):")
-for counter in range(1,10):
-    time_1     = time.time_ns()
-    time_2     = time.time_ns()
-    distance   = lightspeed*(abs(time_1-time_2))/1000000000
-    print(distance)      
+# ==Benchmark time.time() for multiple rounds==
 
+print("Testing time.time() for multiple rounds.")
+
+# Initialize lists with zeros.
+time_1_list       = [0]*loop_count
+time_2_list       = [0]*loop_count
+time_delta        = [0]*loop_count
+time_delta_average = [0]*loop_count
+
+# measure delays in nanoseconds
+for counter in range(1,loop_count):
+    time_1_list[counter] = time.time_ns()
+    time_2_list[counter] = time.time_ns()
+
+# calculate round trip times in nanoseconds
+for counter in range(1,loop_count):
+    time_delta[counter] = (time_2_list[counter] - time_1_list[counter])
+
+# calculate average round trip time in nanoseconds
+sum = 0
+for counter in range(1,loop_count):
+    sum = sum + time_delta[counter]
+    time_delta_average = sum/len(time_delta)
+
+# print average round trip time in nanoseconds
+print("time.time() temporal resolution (nanoseconds):")
+print(time_delta_average)
+    
+# print  upper bound for distance in meters
+print("time.time() spatial resolution (meters):")
+distance = lightspeed*(time_delta_average)/1000000000
+print(distance)
+print("")
+    
+# ==Benchmark time.time_ns() for multiple rounds==
+
+print("Testing time.time_ns() for multiple rounds.")
+
+# Initialize lists with zeros.
+time_1_list       = [0]*loop_count
+time_2_list       = [0]*loop_count
+time_delta        = [0]*loop_count
+time_delta_average = [0]*loop_count
+
+# measure delays in nanoseconds
+for counter in range(1,loop_count):
+    time_1_list[counter] = time.time_ns()
+    time_2_list[counter] = time.time_ns()
+
+# calculate round trip times in nanoseconds
+for counter in range(1,loop_count):
+    time_delta[counter] = (time_2_list[counter] - time_1_list[counter])
+
+# calculate average round trip time in nanoseconds
+sum = 0
+for counter in range(1,loop_count):
+    sum = sum + time_delta[counter]
+    time_delta_average = sum/len(time_delta)
+
+# print average round trip time in nanoseconds
+print("time.time_ns() temporal resolution (nanoseconds):")
+print(time_delta_average)
+    
+# print  upper bound for distance in meters
+print("time.time_ns() spatial resolution (meters):")
+distance = lightspeed*(time_delta_average)/1000000000
+print(distance)
+print("")
 
     
